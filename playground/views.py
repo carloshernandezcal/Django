5ac8980d6d78bf1670fc0f6cae67b4afe689f329
Andres <carloshernandezcal@gmail.com>
2024-11-12 07:50:22 -0500

    uso y funcionamiento de los Templates

--- playground/views.py
+++ playground/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-   return HttpResponse('prueba') 
+   return render(request,'hello.html', {'name': 'Andres'}) 
