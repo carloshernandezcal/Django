fb6beb4b1e0d08d5cfbb06a465dd925737e1d5ce
Andres <carloshernandezcal@gmail.com>
2024-11-13 11:46:03 -0500

    implementacion de la toolbar en la pagina de servidor

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import include, path
+import debug_toolbar
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
+    path('__debug__', include(debug_toolbar.urls)),
 ]
