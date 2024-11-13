6c33bd38bb706ac5a15ebfee7d19556533d38ee8
Andres <carloshernandezcal@gmail.com>
2024-11-12 20:56:20 -0500

    Como funciona un Json para que sirve y utilizacion de debuggin

--- storefront/settings.py
+++ storefront/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground'
-    # 'django.contrib.staticfilesplayground',
-    # 'django.contrib.staticfilesdjango', 
+    
     ]
 
 MIDDLEWARE = [
